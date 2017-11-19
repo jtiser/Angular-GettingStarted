048d16ad03948d8c32be12b621843adcae512f88
Jtiser <julien.tiser@outlook.com>
2017-11-19 15:14:06 +0100

    Get product detail using ProductService and display it

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from '../../api/products/products';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -17,6 +18,11 @@ export class ProductService {
         return this._http.get<IProduct[]>(this._productUrl)
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(err: HttpErrorResponse) {
